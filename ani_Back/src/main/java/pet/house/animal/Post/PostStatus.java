3427519c989df8c6dd6a9ccc222bd49299ff1552
제환웅 <oahfaj@naver.com>
2026-04-16 14:45:01 +0900

    entity 작업

---
+++ ani_Back/src/main/java/pet/house/animal/Post/PostStatus.java
@@ -0,0 +1,18 @@
+package pet.house.animal.Post;
+
+public enum PostStatus {
+    A("ACTIVE(신청)"),
+    C("CANCELLED(취소)"),
+    P("PENDING(대기)"),
+    Y("COMPLETED(완료)");
+
+    private final String description;
+
+    PostStatus(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
